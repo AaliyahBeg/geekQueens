ef870f7e896b2805529f1400e499313cba769659
Chayanika Basak <cb250802@gmail.com>
2022-05-22 14:30:54 +0530

    made responsive

--- client2/src/layouts/Default.layout.jsx
+++ client2/src/layouts/Default.layout.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Footer from "../components/Footer/footer.component";
-// import HeroCarousel from "../components/HeroCarousel/HeroCarousel.component";
 import Navbar from "../components/Navbar/navbar.component";
 import HomePage from "../pages/Home.page";
 
@@ -8,7 +7,6 @@ const DefaultLayout = (props) => {
     return (
     <>
         <Navbar />
-        {/* <HeroCarousel /> */}
         <HomePage/>
         <Footer/>
     </>
